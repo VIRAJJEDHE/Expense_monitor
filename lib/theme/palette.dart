fce7f93c2bee2c0f2ec0af4bc9c84d439cac3379
Viraj Jedhe <virajjedhe786@gmail.com>
2020-11-27 10:15:18 +0530

    final colors

--- lib/theme/palette.dart
+++ lib/theme/palette.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 var dimBlueGrey = Color(Colors.blueGrey.value).withOpacity(0.7);
 var lightGrey = Color.fromRGBO(211, 211, 211, 0.7);
 
-var income = Colors.lightGreen;
+var income = Colors.lightGreenAccent;
 var expense = Colors.red;
 
 var disabled = Color.fromRGBO(90, 90, 90, .4);
